70a67545774a746c7d911a1d419d269cf9b2afa3
JfJubaer <skk5727@gmail.com>
2024-03-11 07:12:37 +0600

    2nd

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application } from "express";
+import cors from "cors";
+// import userRoutes from "./app/Modules/User/user.route";
+
+const app: Application = express();
+
+// using cors
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api/v1/user", userRoutes);
+
+export default app;
